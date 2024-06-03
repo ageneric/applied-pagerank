e82669be5df86bf9fb2d0548d7cccccd796f6262
ageneric-win <55108448+ageneric@users.noreply.github.com>
2024-06-03 17:58:29 +0100

    Clean up pagerank function

--- network-processing-py/pagerank.py
+++ network-processing-py/pagerank.py
@@ -36,17 +36,15 @@ def linear_pagerank(P, v=None, alpha=0.85):
     permutation = np.arange(node_count)[non_dangling_rows]
     counter_permutation = np.arange(node_count)[~non_dangling_rows]
 
+    # Set up the system of linear equations for PageRank
     I = np.identity(P_11.shape[0])
-
     X = (I - alpha*P_11)
     # Solving pi_1^T X = v_1^T <= or equivalently => X^T pi_1 = v_1
     pi_1 = np.linalg.solve(X.transpose(), v_1).transpose()
 
     pi_2 = alpha * pi_1.transpose() @ P_12 + v_2.transpose()
 
-    pi = np.concatenate((pi_1, pi_2))
-
-    # Reverse the permutation
+    # Invert the permutation
     ret = np.empty(node_count)
     ret[permutation] = pi_1
     ret[counter_permutation] = pi_2
