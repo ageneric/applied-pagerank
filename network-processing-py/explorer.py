Implement experimental changes and unfix the fix to RMS weighting

network-processing-py/explorer.py
@@ -8,7 +8,7 @@ Instructions:
     Execute this script in interactive mode with the Python Console
         e.g. run with IDLE
 """
-
+import numpy
 import pandas as pd
 import pickle
 import networkx as nx
@@ -16,7 +16,7 @@ import networkx as nx
 from gene_data import get_gene_differential_expressions
 from weighting import WeightVectorMethod, expressions, TF, TARGET
 from main import generate_networkx_graph
-import pagerank
+from pagerank import linear_pagerank, get_personalisation_vector_by_deg
 import scipy
 
 def get_df():
@@ -26,36 +26,41 @@ def get_df():
 
 def get_network():
     # pickle.dump(network, open('../output/STRING_network.pickle', 'wb'))
-    network = pickle.load(open('../output/product_STRING_network.pickle', 'rb'))
+    network = pickle.load(open('../output/STRING_network.pickle', 'rb'))
     return network
 
 def compute_kl_divergence(p, q):
     """p, q: PageRank vectors."""
-    scipy.special.rel_entr()
+    return sum(scipy.special.rel_entr(p, q))
 
-def compute_difference(order, other_order):
-    """order, other_order: String lists."""
+def compute_rbo(order, other_order):
+    """order, other_order: lists."""
+    return 0
 
-def compute_pagerank_varying_alpha(P):
+def get_pagerank_statistic(P, **kwargs):
+    pagerank = linear_pagerank(P, **kwargs)
+    pagerank_dict = {k: v for k, v in zip(network.nodes, pagerank)}
+
+    genes = [(item[0], item[1]) for item in zip(network.nodes, pagerank)]
+    top_genes = sorted(genes, key=lambda x: -x[1])
+    return pagerank, pagerank_dict, genes, top_genes
+
+def get_pagerank_alpha_difference(P):
     pagerank_collection = {
         'pagerank': [],
         'pagerank_dict': [],
         'genes': [],
         'top_genes': []
     }
-    for alpha in 0.5, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0:
-        pagerank = pagerank.linear_pagerank(matrix_P)
-        pagerank_dict = {k: v for k, v in zip(network.nodes, pagerank)}
-
-        genes = [(item[0], item[1]) for item in zip(network.nodes, pagerank)]
-        top_genes = sorted(genes, key=lambda x: -x[1])
-
-
-        pagerank_collection['pagerank'].append(pagerank)
-        pagerank_collection['genes'].append(genes)
-
+    for alpha in 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0:
+        pagerank_result = get_pagerank_statistic(P, alpha=alpha)
+        # note: this requires the order of pagerank_collection keys
+        # and return order of pagerank statistic to be identical
+        for key, val in zip(pagerank_collection.keys(), pagerank_result):
+            pagerank_collection[key].append(val)
 
     return pagerank_collection
+
 
 if __name__ == '__main__':
     gene_deg = get_gene_differential_expressions(expressions)
@@ -74,10 +79,8 @@ if __name__ == '__main__':
     stochastic_network = nx.stochastic_graph(network)
     matrix_P = nx.adjacency_matrix(stochastic_network, stochastic_network.nodes,
                                    weight='weight').todense()
-    pagerank = pagerank.linear_pagerank(matrix_P)
-    pagerank_dict = {k: v for k, v in zip(network.nodes, pagerank)}
-
-    genes = [(item[0], item[1]) for item in zip(network.nodes, pagerank)]
-    top_genes = sorted(genes, key=lambda x: -x[1])
+    pagerank, pagerank_dict, genes, top_genes = get_pagerank_statistic(matrix_P,
+                                                                       alpha=0.85,
+                                                                       v=get_personalisation_vector_by_deg(network.nodes, gene_deg))
 
     print(f'{top_genes[:25]=}')

network-processing-py/weighting.py
@@ -56,7 +56,7 @@ class WeightMethod:
             # n.b. arbitrary rating for case where no STRING match is found
             return self.DEFAULT_WEIGHT_NO_STRING
 
-    def product_STRING(self, gene_a, gene_b):
+    def RMS_STRING(self, gene_a, gene_b):
         # Use STRING weight and multiply by RMS
         return self.RMS(gene_a, gene_b) * self.STRING(gene_a, gene_b)
 
@@ -65,8 +65,9 @@ class WeightVectorMethod(WeightMethod):
     DEFAULT_WEIGHT_NO_STRING = 0.1
 
     def RMS(self, gene, neighbours):
+        # compute RMS of each pairing of the gene with each of its neighbours
         neighbour_expressions = np.array([self.deg[n] for n in neighbours[TARGET]])
-        return np.sqrt((self.deg[gene]**2 + neighbour_expressions**2) / (len(neighbours) + 1))
+        return np.sqrt((self.deg[gene]**2 + neighbour_expressions**2) / 2)
 
     def STRING(self, gene, neighbours):
         condition = (self.df[TF] == gene)
@@ -78,5 +79,5 @@ class WeightVectorMethod(WeightMethod):
             # n.b. arbitrary rating for case where no STRING match is found
             return self.DEFAULT_WEIGHT_NO_STRING
 
-    def product_STRING(self, gene, neighbours):
+    def RMS_STRING(self, gene, neighbours):
         return self.RMS(gene, neighbours) * self.STRING(gene, neighbours)