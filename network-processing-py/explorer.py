e349a6c7bf6768afb4a6e6baff1b286f93ad17ba
ageneric-win <55108448+ageneric@users.noreply.github.com>
2024-06-17 07:58:37 +0100

    Commit final major changes

--- network-processing-py/explorer.py
+++ network-processing-py/explorer.py
@@ -33,14 +33,16 @@ def get_network():
 
 def get_pagerank_alpha_difference(P, v):
     pagerank_collection = {
+        'pagerank': [],
         'top_genes': []
     }
-    alpha_list = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
+    alpha_list = [0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
     for alpha in alpha_list:
         pagerank = linear_system_pagerank(P, alpha=alpha, v=v)
         _, top_genes = format_pagerank(pagerank, network)
         # note: this requires the order of pagerank_collection keys
         # and return order of pagerank statistic to be identical
+        pagerank_collection['pagerank'].append(pagerank)
         pagerank_collection['top_genes'].append(top_genes)
 
     rbo, kl = [], []
--- network-processing-py/graphic.py
+++ network-processing-py/graphic.py
@@ -29,6 +29,11 @@ def draw_power_law(sorted_values, figure_mode=0, text_values=None, tex=False):
     plt.ylabel(yl)
     plt.title(title)
 
+    if figure_mode % 5:
+        for i in range(0, 5):
+            line = plt.axvline(x=10**i, linestyle='-')
+            line.set_color("white")
+
     if figure_mode % 2:
         sorted_values = sorted_values[:10000]
 
@@ -36,13 +41,28 @@ def draw_power_law(sorted_values, figure_mode=0, text_values=None, tex=False):
 
     if figure_mode % 3:
         space = 1.1 ** np.arange(0, 98)
-        plt.plot(space, space ** 1.25)
+        plt.plot(space, space ** 1.32 * sorted_values[0] * 1.2)
+        cont_space = 1.1 ** np.arange(98, 100)
+        plt.plot(cont_space, cont_space ** 1.32 * sorted_values[0] * 1.2,
+                 linestyle='dotted')
 
-    plt.grid(True, which='both', linestyle='--', linewidth=0.25)
+    plt.grid(True, which='both')
     plt.show()
 
 def draw_gene_deg_plot(gene_deg, top_gene_names):
     plt.plot([gene_deg[g] for g in top_gene_names[:1500]])
+    plt.show()
+
+def draw_sorted_gene_deg_plot(gene_deg):
+    plt.figure(figsize=(10, 6))
+
+    plt.xlabel('Gene Differential Expression')
+    plt.ylabel('Gene Index')
+    plt.title('Plot of Gene Differential Expressions')
+    plt.grid(True, which='both')
+    space = 1.1 ** np.arange(0, 98)
+    plt.plot(space, (space ** 1.33) / 1000)
+    plt.plot(sorted(gene_deg.values()))
     plt.show()
 
 def draw_divergence_plot(pagerank_collection):
@@ -51,14 +71,45 @@ def draw_divergence_plot(pagerank_collection):
 def draw_robustness_region(pagerank_collection):
     plt.show()
 
+def draw_pagerank_hist(pageranks):
+    plt.hist(sorted(pageranks, key=lambda x: -x), bins=100)
+    plt.xlabel('PageRank')
+    plt.ylabel('Frequency Density')
+    plt.title('Histogram of Gene Differential Expressions (PageRank < 0.0001)')
+    plt.show()
+
+
+
 def write_to_graphml(network, pagerank_dict, gene_deg, name='graph'):
     import pathlib
 
     nx.set_node_attributes(network, gene_deg, name='differential_expression')
     nx.set_node_attributes(network, pagerank_dict, name='pagerank')
 
-    attrs = []
-    for edge in network.edges:
-        attrs.append(pagerank_dict, [edge[1]])
+    attrs = {}
+    ws = nx.get_edge_attributes(network, 'weight')
+    ws3 = {k: v*v for k, v in ws.items()}
 
+    for i, edge in enumerate(network.edges):
+        attrs[edge] = 1000 * pagerank_dict[edge[0]] * pagerank_dict[edge[1]] * ws3[edge]
+    nx.set_edge_attributes(network, attrs, 'pagerank_weight')
+
+    nx.set_edge_attributes(network, ws3, 'rescaled_weight')
     nx.write_graphml(network, pathlib.Path.cwd() / (name + '.graphml'))
+
+
+def alpha_plot(alphas, values, text_values=None, tex=False):
+    if text_values is None:
+        xl = 'Alpha'
+        yl = 'K-L Divergence with alpha = 0.85'
+        title = 'K-L Divergence as Teleportation Probability (alpha) Varies'
+    else:
+        xl, yl, title = text_values
+
+    plt.figure(figsize=(10, 6))
+    plt.ylim([-0.05, 0.5])
+    plt.xlabel(xl)
+    plt.ylabel(yl)
+    plt.title(title)
+
+    plt.plot(alphas, values)
